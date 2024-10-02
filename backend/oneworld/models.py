b19cea2be9590e799c8c7be445846f9342b42637
kiptootitus <kiptootitus75@gmail.com>
2024-10-02 16:58:39 +0300

    setup

--- backend/oneworld/models.py
+++ backend/oneworld/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+
+    def __str__(self):
+        return self.user.username
+      
+class OTP(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.IntegerField()
+    count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.user.username
+      
+    def is_valid(self):
+        return self.count < 3 and self.expires_at > timezone.now()
+      
+    @staticmethod
+    def get_valid_otp(user):
+        otp = OTP.objects.filter(user=user).order_by('-created_at').first()
+        if otp and otp.is_valid():
+            return otp
+        return None
+      
+class SendEmail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
+    def is_valid(self):
+        return self.created_at > timezone.now() - timezone.timedelta(days=1)
+class VerifyEmail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    token = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
+    def is_valid(self):
+        return self.created_at > timezone.now() - timezone.timedelta(days=1)
+      
--- backend/oneworld/views.py
+++ backend/oneworld/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.views.generic import FormView
+from django.views.generic import RedirectView
+from django.views.generic import ArchiveIndexView
 
-# Create your views here.
+
+
+class UserRegesterGenericView(CreateView):
+
+    def form_valid(self, form):
+        form.save()
+        return super(UserRegesterGenericView, self).form_valid(form)
